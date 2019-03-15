2e6b515cfa343eef727c68e3fae05f353ecd1dbb
Caleb Smith <calebfrances@msn.com>
2019-03-15 16:08:06 -0500

    Add backend routes. Add basic server setup and connection. Add controller skeletons for endpoints.

---
+++ server/controllers/controller.js
@@ -0,0 +1,6 @@
+module.exports = {
+	get_hotel: (req, res) => {},
+	get_room: (req, res) => {},
+	get_reservation: (req, res) => {},
+	new_transaction: (req, res) => {}
+};
--- server/index.js
+++ server/index.js
@@ -1,1 +1,31 @@
-//This is a fantastic serverles server
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const massive = require('massive');
+const { json } = require('body-parser');
+const controller = require('./controllers/controller');
+
+app.use(json());
+
+//Database connection
+massive(process.env.CONNECTION_STRING)
+	.then(dbInstance => {
+		app.set('db', dbInstance);
+		console.log('db connected');
+	})
+	.catch(err => console.log(err));
+
+//Hotel and toom endpoints
+app.get('/api/hotel', controller.get_hotel);
+app.get('/api/roomlist', controller.get_room);
+
+//Reservation endpoints
+app.get('/api/reservation', controller.get_reservation);
+
+//Transaction endpoints
+app.post('./api/transaction', controller.new_transaction);
+//User endpoints
+
+app.listen(process.env.EXPRESS_PORT, () => {
+	console.log(`Server - Listening on ${process.env.EXPRESS_PORT}`);
+});
