6f380ad11098a27451e9cb92e3ec4a81544e92d0
Zaid <sourcing.zaidhassan@gmail.com>
2019-03-15 16:07:34 -0500

    added routes in routes.js and in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import routes from "../routes";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hi</h1>
-      </div>
+      <Router>
+        <div className="App">{routes}</div>
+      </Router>
     );
   }
 }
