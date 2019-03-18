80571baebb218c2fb38d8e1a1fc73f29875b3349
Zaid <sourcing.zaidhassan@gmail.com>
2019-03-18 16:52:42 -0500

    restructure homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -14,17 +14,24 @@ import Paper from "@material-ui/core/Paper";
 import classNames from "classnames";
 
 const styles = theme => ({
+  entireContainer: {
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
   background: {
-    height: "50vh",
-    width: "70vw",
-    margin: "auto"
+    height: "60vh",
+    width: "100vw"
   },
   searchContainer: {
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
+    width: "70vw"
   },
   textField: {
-    width: 470
+    width: 470,
+    marginLeft: "5vw"
   },
   paper: {
     width: "70vw",
@@ -39,36 +46,43 @@ const styles = theme => ({
     width: 100
   },
   button: {
-    height: 30,
-    width: 30,
+    height: 40,
+    width: 40,
     borderRadius: 8,
     background: theme.palette.accent.main,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    margin: 20
+    margin: 15
   },
   selectors: {
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
+    margin: "0 auto",
+    justifyContent: "center",
+    marginTop: 15,
+    marginLeft: 50
   },
   rightSearchContainer: {
-    width: "35vw"
+    width: "35vw",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly"
   },
   counter: {
     display: "flex",
-    alignItems: "center"
+    alignItems: "center",
+    width: "12vw"
   },
   leftSearchContainer: {
-    width: "35vw",
-    marginLeft: 200
+    width: "32vw"
   }
 });
 
 const HomePage = props => {
   const { classes } = props;
   return (
-    <div className={classes.background}>
+    <div className={classes.entireContainer}>
       <img
         className={classes.background}
         src="https://images.unsplash.com/photo-1502920514313-52581002a659?ixlib=rb-1.2.1&auto=format&fit=crop&w=1047&q=80"
@@ -125,6 +139,7 @@ const HomePage = props => {
           <div className={classes.rightSearchContainer}>
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
               <DatePicker
+                className={classes.datepicker}
                 margin="normal"
                 label="Check-In"
                 // value={selectedDate}
@@ -139,6 +154,7 @@ const HomePage = props => {
                 }}
               />
               <DatePicker
+                className={classes.datepicker}
                 margin="normal"
                 label="Check-Out"
                 // value={selectedDate}
@@ -153,6 +169,7 @@ const HomePage = props => {
                 }}
               />
             </MuiPickersUtilsProvider>
+            <button> Search </button>
           </div>
         </div>
       </Paper>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,51 @@ import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
   navContainer: {
-    height: "5vh"
+    // height: "10vh",
+    // width: "90vw",
+    // margin: "auto",
+    // background: "#96BBBB",
+    // marginBottom: 5,
+    // display: "flex",
+    // justifyContent: "space-between",
+    // borderRadius: 8,
+    position: "fixed",
+    top: 0,
+    left: 0,
+    height: "100vh",
+    width: "4vw",
+    background: theme.palette.primary.main
+  },
+  logo: {
+    height: "10vh",
+    width: "3vw",
+    textAlign: "center",
+    background: theme.palette.accent.main,
+    padding: 9,
+    paddingTop: 50
+  },
+  logoText: {
+    textAlign: "center",
+    paddingTop: "4vh"
+  },
+  lists: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    flexDirection: "column",
+    height: "50vh",
+    width: "6vw"
+  },
+  navwrapper: {
+    overflowX: "hidden",
+    cursor: "pointer"
+  },
+  tags: {
+    borderBottom: "1px solid #96BBBB",
+    textAlign: "center",
+    position: "relative",
+    textDecoration: "none",
+    verticalAlign: "middle",
+    height: "6vh"
   }
 });
 
@@ -11,7 +55,17 @@ const Nav = props => {
   const { classes } = props;
   return (
     <div className={classes.navContainer}>
-      <h1>Away From Home</h1>
+      <div className={classes.logo}>
+        <h1 className={classes.logoText}>Away From Home</h1>
+      </div>
+      <div className={classes.navwrapper}>
+        <nav className={classes.lists}>
+          <a className={classes.tags}>Sign In</a>
+          <a className={classes.tags}>Sign Up</a>
+          <a className={classes.tags}>Locations</a>
+          <a className={classes.tags}>Offers </a>
+        </nav>
+      </div>
     </div>
   );
 };
