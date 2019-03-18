c699647d1683a6bf9137b9cc32a16200c929e9cc
Dylan Lee <csleedylan@gmail.com>
2019-03-18 16:56:06 -0500

    add user info section, add credit info section, add checkout info section, add stripe, piece it together in checkout.js

--- src/components/Checkout/CreditInfo.js
+++ src/components/Checkout/CreditInfo.js
@@ -1,11 +1,62 @@
 import React from "react";
+import { CardElement, injectStripe } from "react-stripe-elements";
+import { withStyles } from "@material-ui/core/styles";
 
-const CreditInfo = () => {
+const styles = theme => ({
+  root: {
+    margin: "1vh"
+  },
+  input: {
+    width: "25vw",
+    padding: "10px",
+    border: "1px solid grey",
+    borderRadius: "6px"
+  },
+  submit: {
+    width: "4vw"
+  },
+  stripe: {
+    display: "flex"
+  },
+  title: {
+    fontSize: "1.5em",
+    marginBottom: "1vh"
+  }
+});
+
+const CreditInfo = props => {
+  const { classes } = props;
+  const submit = async ev => {
+    let { token } = await props.stripe.createToken({ name: "Name" });
+    console.log(token);
+    let response = await fetch("/charge", {
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: token.id
+    });
+
+    if (response.ok) console.log("Purchase Complete!");
+  };
   return (
-    <div>
-      <h1>CreditInfo</h1>
+    <div className={classes.root}>
+      <p className={classes.title}>Card Information</p>
+      <div className={classes.stripe}>
+        <CardElement
+          className={classes.input}
+          style={{
+            base: {
+              lineHeight: "45px",
+              fontWeight: 300,
+              fontSize: "20px"
+            }
+          }}
+        />
+        <button className={classes.submit} onClick={submit}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
 
-export default CreditInfo;
+export default injectStripe(withStyles(styles)(CreditInfo));
--- src/components/Checkout/UserInfo.js
+++ src/components/Checkout/UserInfo.js
@@ -1,32 +1,58 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import CreditInfo from "./CreditInfo";
+import { Elements, StripeProvider } from "react-stripe-elements";
 
 const styles = theme => ({
-  root: {
+  background: {
     background: theme.palette.secondary.main,
-    display: "flex",
-    flexDirection: "column",
-    width: "30vw"
+    width: "35vw"
   },
-  name: {}
+  root: {
+    margin: "1vw",
+    display: "flex",
+    flexDirection: "column"
+  },
+  name: {},
+  header: {
+    fontSize: "1.5em",
+    marginBottom: "2vh"
+  },
+  firstP: {
+    marginBottom: "2vh"
+  }
 });
 
 const UserInfo = props => {
   const { classes } = props;
   return (
-    <div className={classes.root}>
-      <div className={classes.name}>
-        <TextField label="First Name" margin="normal" variant="outlined" />
-        <TextField label="Last Name" margin="normal" variant="outlined" />
+    <Paper className={classes.background}>
+      <div className={classes.root}>
+        <h1 className={classes.header}>Guest Information</h1>
+        <p className={classes.firstP}>
+          Already an IHG Rewards Club member? Sign in to earn your points and
+          save time with automatic form completion.
+        </p>
+        <p>* Indicates a required input field</p>
+        <div className={classes.name}>
+          <TextField label="First Name*" margin="normal" variant="outlined" />
+          <TextField label="Last Name*" margin="normal" variant="outlined" />
+        </div>
+        <TextField label="Phone Number*" margin="normal" variant="outlined" />
+        <TextField label="Email Address*" margin="normal" variant="outlined" />
+        <TextField label="Address*" margin="normal" variant="outlined" />
+        <TextField label="City/Town*" margin="normal" variant="outlined" />
+        <TextField label="Postal Code*" margin="normal" variant="outlined" />
+        <TextField label="Country/Region*" margin="normal" variant="outlined" />
+        <StripeProvider apiKey="pk_test_g0IpuPbZCFN2PaAxLnEfDPng">
+          <Elements>
+            <CreditInfo />
+          </Elements>
+        </StripeProvider>
       </div>
-      <TextField label="Phone Number" margin="normal" variant="outlined" />
-      <TextField label="Email Address" margin="normal" variant="outlined" />
-      <TextField label="Address" margin="normal" variant="outlined" />
-      <TextField label="City/Town" margin="normal" variant="outlined" />
-      <TextField label="Postal Code" margin="normal" variant="outlined" />
-      <TextField label="Country/Region" margin="normal" variant="outlined" />
-    </div>
+    </Paper>
   );
 };
 
