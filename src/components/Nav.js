b8da9670bc23f261d63534fbeb67c376084be27b
Zaid <sourcing.zaidhassan@gmail.com>
2019-03-18 21:29:38 -0500

    review navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,35 +11,49 @@ const styles = theme => ({
     background: "#4C525A"
   },
   logo: {
-    height: "10vh",
     textAlign: "center",
-    background: theme.palette.accent.main,
-    height: "15vh",
+    height: "12vh",
     borderBottom: "1px solid #96BBBB",
-    paddingTop: "7.5vh",
-    color: "#fff"
+    color: "#fff",
+    lineHeight: "12vh",
+    background: theme.palette.secondary.main
+  },
+  navwrapper: {
+    overflowY: "hidden",
+    cursor: "pointer",
+    height: "55vh"
   },
   lists: {
     display: "flex",
     justifyContent: "space-evenly",
     flexDirection: "column",
-    height: "50vh"
-  },
-  navwrapper: {
-    overflowX: "hidden",
-    cursor: "pointer"
+    height: "55vh"
   },
   tags: {
     borderBottom: "1px solid #96BBBB",
     textAlign: "center",
-    position: "relative",
     textDecoration: "none",
-    verticalAlign: "middle",
-    height: "6vh",
     textTransform: "uppercase",
     fontSize: 13,
     color: "#fff",
-    fontFamily: "Baloo"
+    fontFamily: "Baloo",
+    height: "15vh",
+    margin: "0 auto",
+    width: "5vw",
+    lineHeight: 9.5,
+    background: `linear-gradient(to right,
+      #CCFF33,
+      #CCFF33 50%,
+      transparent 50%,
+      transparent)`,
+    backgroundPosition: "100% 0",
+    backgroundSize: "200% 100%",
+    transition: "all .3s ease-in",
+    "&:hover": {
+      color: " #006600",
+      borderBottom: "1px #006600 dotted",
+      backgroundPosition: "0 0"
+    }
   }
 });
 
@@ -47,7 +61,7 @@ const Nav = props => {
   const { classes } = props;
   return (
     <div className={classes.navContainer}>
-      <div className={classes.logo}>Away From Home</div>
+      <div className={classes.logo}>AFM</div>
       <div className={classes.navwrapper}>
         <nav className={classes.lists}>
           <a className={classes.tags}>Sign In</a>
@@ -61,3 +75,20 @@ const Nav = props => {
 };
 
 export default withStyles(styles)(Nav);
+
+// position: "absolute"
+// "&:after": {
+//   position: "absolute",
+//   content: "''",
+//   top: 0,
+//   bottom: 0,
+//   left: 0,
+//   height: "100%",
+//   transformOrigin: "left",
+//   transition: "5s",
+//   zIndex: -1,
+//   background: "red"
+// },
+// "&:hover:after": {
+//   width: "100%"
+// }
