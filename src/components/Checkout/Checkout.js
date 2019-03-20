02a1a6951f2a64e6a220deb5115747ab5673b8ab
Dylan Lee <csleedylan@gmail.com>
2019-03-20 16:29:47 -0500

    add useInput hook in Userinfo.js. add register user checkbox in checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,68 +1,68 @@
-import React from "react";
-import CheckoutInfo from "./CheckoutInfo";
-import UserInfo from "./UserInfo";
-import SearchInfo from "../Search/SearchInfo";
-import { withStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+import React from 'react';
+import CheckoutInfo from './CheckoutInfo';
+import UserInfo from './UserInfo';
+import SearchInfo from '../Search/SearchInfo';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 
 const styles = theme => ({
-  root: {
-    display: "flex",
-    flexDirection: "column"
-  },
-  upper: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  info: {
-    background: theme.palette.tertiary.main,
-    width: "61vw",
-    marginTop: "1vh",
-    marginBottom: "1vh"
-  },
-  innerInfo: {
-    margin: "1vw"
-  },
-  second: {
-    display: "flex",
-    marginBottom: "2vh"
-  },
-  upperHeader: {
-    fontSize: "1.3em"
-  },
-  upperPrice: {
-    fontSize: "1.7em"
-  },
-  filler: {
-    height: "20vh"
-  }
+   root: {
+      display: 'flex',
+      flexDirection: 'column'
+   },
+   upper: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+   },
+   info: {
+      background: theme.palette.tertiary.main,
+      width: '61vw',
+      marginTop: '1vh',
+      marginBottom: '1vh'
+   },
+   innerInfo: {
+      margin: '1vw'
+   },
+   second: {
+      display: 'flex',
+      marginBottom: '2vh'
+   },
+   upperHeader: {
+      fontSize: '1.3em'
+   },
+   upperPrice: {
+      fontSize: '1.7em'
+   },
+   filler: {
+      height: '20vh'
+   }
 });
 
 const Checkout = props => {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <SearchInfo />
-      <div className={classes.upper}>
-        <Paper className={classes.info}>
-          <div className={classes.innerInfo}>
-            <h1 className={classes.upperHeader}>Two Queen Beds Nonsmoking</h1>
-            <h2 className={classes.upperHeader}>Rate Description</h2>
-            <h3># of Rooms left</h3>
-            <h2 className={classes.upperPrice}>Total Price for stay</h2>
-          </div>
-        </Paper>
-        <div className={classes.second}>
-          <UserInfo />
-          <CheckoutInfo />
-        </div>
+   const { classes } = props;
+   return (
+      <div className={classes.root}>
+         <SearchInfo />
+         <div className={classes.upper}>
+            <Paper className={classes.info}>
+               <div className={classes.innerInfo}>
+                  <h1 className={classes.upperHeader}>Two Queen Beds Nonsmoking</h1>
+                  <h2 className={classes.upperHeader}>Rate Description</h2>
+                  <h3># of Rooms left</h3>
+                  <h2 className={classes.upperPrice}>Total Price for stay</h2>
+               </div>
+            </Paper>
+            <div className={classes.second}>
+               <UserInfo />
+               <CheckoutInfo />
+            </div>
+         </div>
+         <div className={classes.filler}>
+            <h1>Hey this makes the page longer</h1>
+         </div>
       </div>
-      <div className={classes.filler}>
-        <h1>Hey this makes the page longer</h1>
-      </div>
-    </div>
-  );
+   );
 };
 
 export default withStyles(styles)(Checkout);
--- src/components/Checkout/CreditInfo.js
+++ src/components/Checkout/CreditInfo.js
@@ -1,62 +1,66 @@
-import React from "react";
-import { CardElement, injectStripe } from "react-stripe-elements";
-import { withStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { CardElement, injectStripe } from 'react-stripe-elements';
+import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
-  root: {
-    margin: "1vh"
-  },
-  input: {
-    width: "25vw",
-    padding: "10px",
-    border: "1px solid grey",
-    borderRadius: "6px"
-  },
-  submit: {
-    width: "4vw"
-  },
-  stripe: {
-    display: "flex"
-  },
-  title: {
-    fontSize: "1.5em",
-    marginBottom: "1vh"
-  }
+   root: {
+      margin: '1vh'
+   },
+   input: {
+      padding: '10px',
+      border: '1px solid grey',
+      borderRadius: '5px'
+   },
+   submit: {
+      height: '5vh',
+      marginTop: '1vh',
+      fontSize: '1.5em',
+      background: theme.palette.accent.main,
+      border: 'none',
+      borderRadius: '5px'
+   },
+   stripe: {
+      display: 'flex',
+      flexDirection: 'column'
+   },
+   title: {
+      fontSize: '1.5em',
+      marginBottom: '1vh'
+   }
 });
 
 const CreditInfo = props => {
-  const { classes } = props;
-  const submit = async ev => {
-    let { token } = await props.stripe.createToken({ name: "Name" });
-    console.log(token);
-    let response = await fetch("/charge", {
-      method: "POST",
-      headers: { "Content-Type": "text/plain" },
-      body: token.id
-    });
+   const { classes } = props;
+   const submit = async ev => {
+      let { token } = await props.stripe.createToken({ name: 'Name' });
+      let response = await fetch('/charge', {
+         method: 'POST',
+         headers: { 'Content-Type': 'text/plain' },
+         body: token.id
+      });
 
-    if (response.ok) console.log("Purchase Complete!");
-  };
-  return (
-    <div className={classes.root}>
-      <p className={classes.title}>Card Information</p>
-      <div className={classes.stripe}>
-        <CardElement
-          className={classes.input}
-          style={{
-            base: {
-              lineHeight: "45px",
-              fontWeight: 300,
-              fontSize: "20px"
-            }
-          }}
-        />
-        <button className={classes.submit} onClick={submit}>
-          Submit
-        </button>
+      if (response.ok) console.log('Purchase Complete!');
+   };
+   return (
+      <div className={classes.root}>
+         <p className={classes.title}>Card Information</p>
+         <div className={classes.stripe}>
+            <CardElement
+               className={classes.input}
+               style={{
+                  base: {
+                     lineHeight: '45px',
+                     fontWeight: 300,
+                     fontSize: '20px'
+                  }
+               }}
+            />
+            <button className={classes.submit} onClick={submit}>
+               Create Reservation
+            </button>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default injectStripe(withStyles(styles)(CreditInfo));
