f8e4fdb762f18d42368037504003045142383c77
Caleb Smith <calebfrances@msn.com>
2019-03-21 09:26:10 -0500

    package.lock update

--- src/theme.js
+++ src/theme.js
@@ -1,36 +1,34 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
-   overrides: {
-      MuiInputBase: {
-         input: {
-            textAlign: 'left'
-         }
-      },
-      MuiOutlinedInput: {
-         input: {
-            margin: 10
-         }
-      }
-    }
-  },
-  palette: {
-    primary: {
-      main: "#474056",
-      second: "#F2E3BC"
-    },
-    secondary: {
-      main: "#96BBBB"
-    },
-    tertiary: {
-      main: "#656D79"
-    },
-    accent: {
-      main: "#9BA2FF"
-    }
-  },
-  typography: { useNextVariants: true }
-
+	overrides: {
+		MuiInputBase: {
+			input: {
+				textAlign: 'left'
+			}
+		},
+		MuiOutlinedInput: {
+			input: {
+				margin: 10
+			}
+		}
+	},
+	palette: {
+		primary: {
+			main: '#474056',
+			second: '#F2E3BC'
+		},
+		secondary: {
+			main: '#96BBBB'
+		},
+		tertiary: {
+			main: '#656D79'
+		},
+		accent: {
+			main: '#9BA2FF'
+		}
+	},
+	typography: { useNextVariants: true }
 });
 
 export default theme;
