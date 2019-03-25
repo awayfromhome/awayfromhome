dbf75c767fe294ea217df96be2c1d1906b90c4d4
Caleb Smith <calebfrances@msn.com>
2019-03-25 16:18:15 -0500

    add search bar connection to data inflow from search info, add search info to reducer, sync, and constants. Add tweaked styling on search bar to accomodate new information.

--- src/components/Search/SearchInfo.js
+++ src/components/Search/SearchInfo.js
@@ -6,105 +6,123 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles(theme => ({
-  appBar: {
-    flexGrow: 1,
-    textAlign: 'center',
-    background: theme.palette.tertiary.main,
-    width: '61vw'
-  },
-  barInfo: {
-    display: 'flex',
-    justifyContent: 'space-between'
-  },
-  toolbar: {
-    width: '60vw'
-  },
-  city: {
-    width: '30vw'
-  },
-  searchInfo: {
-    marginLeft: 10
-  },
-  divider: {
-    marginLeft: '1%'
-  },
-  deatilsInfo: {
-    width: '100%',
-    margin: 'auto'
-  },
-  [theme.breakpoints.down('749')]: {
-    appBar: {
-      display: 'flex',
-      height: '15vh',
-      width: '100%'
-    },
-    toolbar: {
-      width: '100vw',
-      display: 'flex',
-      flexDirection: 'column',
-      marginLeft: 0,
-      margin: 'auto'
-    },
-    city: {
-      width: 'auto'
-    },
-    barInfo: {
-      display: 'block',
-      width: '100%'
-    },
-    deatilsInfo: {
-      fontSize: '15px'
-    },
-    divider: {
-      margin: '2%'
-    },
-    searchInfo: {
-      marginLeft: 0
-    }
-  },
-  ['@media (min-width: 750px) and (max-width: 1200px)']: {
-    toolbar: {
-      width: 'auto'
-    },
-    deatilsInfo: {
-      fontSize: '15px',
-      width: '50vw'
-    },
-    city: {
-      fontSize: '17px',
-      width: '100%'
-    },
-    appBar: {
-      margin: 'auto',
-      width: '61vw'
-    },
-    barInfo: {
-      display: 'block',
-      margin: 'auto'
-    }
-  }
+	appBar: {
+		flexGrow: 1,
+		textAlign: 'center',
+		background: theme.palette.tertiary.main,
+		width: '63vw'
+	},
+	barInfo: {
+		display: 'flex',
+		justifyContent: 'space-around'
+	},
+	toolbar: {
+		width: '60vw'
+	},
+	city: {
+		width: '20vw'
+	},
+	searchInfo: {
+		marginLeft: 10
+	},
+	divider: {
+		marginLeft: '1%'
+	},
+	detailsInfo: {
+		width: '35vw',
+		margin: 'auto'
+	},
+	[theme.breakpoints.down('749')]: {
+		appBar: {
+			display: 'flex',
+			height: '15vh',
+			width: '100%'
+		},
+		toolbar: {
+			width: '100vw',
+			display: 'flex',
+			flexDirection: 'column',
+			marginLeft: 0,
+			margin: 'auto'
+		},
+		city: {
+			width: 'auto'
+		},
+		barInfo: {
+			display: 'block',
+			width: '100%'
+		},
+		detailsInfo: {
+			width: '100%',
+			fontSize: '15px'
+		},
+		divider: {
+			margin: '2%'
+		},
+		searchInfo: {
+			marginLeft: 0
+		}
+	},
+	['@media (min-width: 750px) and (max-width: 1200px)']: {
+		toolbar: {
+			width: 'auto'
+		},
+		detailsInfo: {
+			fontSize: '15px',
+			width: '50vw'
+		},
+		city: {
+			fontSize: '17px',
+			width: '100%'
+		},
+		appBar: {
+			margin: 'auto',
+			width: '61vw'
+		},
+		barInfo: {
+			display: 'block',
+			margin: 'auto'
+		}
+	}
 }));
 
 const SearchInfo = props => {
-  const classes = useStyles();
-  return (
-    <AppBar position='static' className={classes.appBar}>
-      <Toolbar className={classes.toolbar}>
-        <Typography className={classes.barInfo} variant='h6' color='inherit'>
-          <div className={classes.city}>Dallas, Tx, United States</div>
-          <div className={classes.deatilsInfo}>
-            <span className={classes.searchInfo}>
-              03/18/2019 - 03/19/2019 <span className={classes.divider}> |</span>
-            </span>
-            <span className={classes.searchInfo}>
-              # of Guests <span className={classes.divider}> |</span>
-            </span>
-            <span className={classes.searchInfo}> 1 room </span>
-          </div>
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
+	console.log('search info', props.setSearchInfo);
+	const {
+		destination,
+		occupants,
+		rooms,
+		firstDate,
+		secondDate
+	} = props.setSearchInfo;
+	const classes = useStyles();
+	return (
+		<AppBar position="static" className={classes.appBar}>
+			<Toolbar className={classes.toolbar}>
+				<Typography
+					className={classes.barInfo}
+					variant="h6"
+					color="inherit">
+					<div className={classes.city}>
+						{destination}, TX, United States
+					</div>
+					<div className={classes.detailsInfo}>
+						<span className={classes.searchInfo}>
+							{firstDate} - {secondDate}
+							<span className={classes.divider}> |</span>
+						</span>
+						<span className={classes.searchInfo}>
+							Guests: {occupants}
+							<span className={classes.divider}> |</span>
+						</span>
+						<span className={classes.searchInfo}>
+							Rooms: {rooms}
+						</span>
+					</div>
+				</Typography>
+			</Toolbar>
+		</AppBar>
+	);
 };
 
 const mapStatetoProps = state => state;
