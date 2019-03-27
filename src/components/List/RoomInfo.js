1aabf81141b4ea0eed2318a03ad00868a9899478
Dylan Lee <csleedylan@gmail.com>
2019-03-27 17:23:48 -0500

    add styling in roomlist, applied the get reservation to the front end

--- src/components/List/RoomInfo.js
+++ src/components/List/RoomInfo.js
@@ -6,11 +6,10 @@ import { withRouter } from 'react-router-dom';
 const useStyles = makeStyles(theme => ({
   roomInfo: {
     height: '80%',
-    margin: 'auto',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-evenly',
-    marginLeft: '5vw'
+    marginLeft: '2vw'
   },
   roomDescription: {
     width: '70%'
@@ -31,24 +30,14 @@ const useStyles = makeStyles(theme => ({
 
 const RoomInfo = props => {
   const { info } = props;
+  console.log(info);
   const classes = useStyles();
   return (
-    <Card
-      price='$97'
-      img={info.url}
-      onClick={() => props.history.push(`/Checkout`)}
-      btnName='Select Room'
-    >
-      {/* <h1>{props.info.room_id}</h1> */}
+    <Card price={info.price} img={info.url} onClick={() => props.history.push(`/Checkout`)} btnName='Select Room'>
       <div className={classes.roomInfo}>
-        <h1 className={classes.numberOfRooms}>Only 1 Room Left</h1>
-        <h1 className={classes.roomTitle}>1 King Bed Leisure Nonsmoking</h1>
-        <p className={classes.roomDescription}>
-          Understand the true meaning of contentment in this rm. Stay productive
-          at the well lit desk. Relax in the ergonomic chair with free high
-          speed internet or let indulgence rain all over you from the a
-          pulsating showerhead.
-        </p>
+        <h1 className={classes.numberOfRooms}>There are {info.number_of_rooms} rooms left</h1>
+        <h1 className={classes.roomTitle}>{info.room_name}</h1>
+        <p className={classes.roomDescription}>{info.description}</p>
       </div>
     </Card>
   );
